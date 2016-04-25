a89470c289d01266521ebe82ffcfe795b3abacd6
rokusaki <rokusaki34@gmail.com>
2016-04-25 10:08:50 +0700

    Add DOTween
    - Add DOTween
    - Add first Script <Main Equipment>

---
+++ Assets/Script/Equipment/MainEquipmentClass.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainEquipmentClass {
+
+    private int hp;
+    private int mp;
+    private int strength;
+    private int intelligent;
+    private int vitality;
+    private int agility;
+
+    private int attack;
+    private int magicAttack;
+    private int defense;
+    private int magicDefense;
+    private int speed;
+
+    private System.Action options;
+
+
+    void Update()
+    {
+        
+    }
+
+}
